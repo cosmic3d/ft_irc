205a696bacb6aa3a2328a0caa470335ce1df1b97
David Mendez Carrillo <50409052+damendez00@users.noreply.github.com>
2024-08-21 08:54:44 +0000

    server and client structure added

--- hdrs/Server.hpp
+++ hdrs/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: damendez <damendez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:45:32 by damendez          #+#    #+#             */
-/*   Updated: 2024/08/16 12:13:35 by damendez         ###   ########.fr       */
+/*   Updated: 2024/08/21 08:49:54 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 #include <arpa/inet.h>
 #include <poll.h>
 #include <cstring>
+#include "Client.hpp"
 
 #define PORT 8080
 #define MAX_CLIENTS 10
@@ -30,17 +31,19 @@ class Server {
         int                     _serverSocket; // fd for listening socket
         int                     _port; // port number (identifies processes)
         std::string             _password;
-        std::vector<pollfd>     _pollFds;
-        //std::map<int, Client*>            _clients; // TO-DO
+        std::vector<pollfd>     _pollFds; // monitored by the server for events using poll()
+        std::map<int, Client*>            _clients; // TO-DO
         //std::map<std::string, Channel*>   _channels: // TO-DO
         
     public:
         Server(int port, const std::string &password);
         ~Server();
-
+        
+        void    start();
         void    init();
         void    run();
-        void    handleConnection(); // TO-DO
+        
+        void    handleConnection();
         void    handleClient(int clientSocket); // TO-DO
         void    handleAuthentication(int clientSocket, const std::string &message); // TO-DO
         void    parseCommand(int clientSocket, const std::string &message);// TO-DO
--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,24 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <cstring>
-#include <arpa/inet.h>
+#include "Server.hpp"
+#include "Client.hpp"
+//#include "Channel.hpp"
+//#include "Request.hpp"
 
-const std::vector<std::string> supported_capabilities = 
-		std::vector<std::string>(1, "account-notify");
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+        return 1;
+    }
 
-void handle_client(int client_fd, const std::string& password) {
-		char buffer[1024];
-		std::string client_message;
-		
-		// Build the list of supported capabilities
-		std::string capabilities;
-		for (std::vector<std::string>::const_iterator it = supported_capabilities.begin(); it != supported_capabilities.end(); ++it) {
-				if (!capabilities.empty()) {
-						capabilities += " ";
-				}
-				capabilities += *it;
-		}
-		
-		std::string cap_ls_response = ":server.capabilities CAP * LS :" + capabilities + "\r\n";
-		send(client_fd, cap_ls_response.c_str(), cap_ls_response.length(), 0);
+    int port = std::stoi(argv[1]);
+    std::string password = argv[2];
 
-		// Read messages from the client
-		while (true) {
-				memset(buffer, 0, sizeof(buffer));
-				ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+    try {
+        Server ircServer(port, password);
+        ircServer.start();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 
-				if (bytes_received <= 0) {
-						std::cerr << "Client disconnected or error occurred" << std::endl;
-						close(client_fd);
-						break;
-				}
-
-				client_message = std::string(buffer);
-
-				if (client_message.find("CAP LS") != std::string::npos) {
-						// Handle CAP LS command
-						std::string cap_response = ":server.capabilities CAP * LS :" + capabilities + "\r\n";
-						send(client_fd, cap_response.c_str(), cap_response.length(), 0);
-				} else if (client_message.find("CAP REQ") != std::string::npos) {
-						// Handle CAP REQ command
-						std::string cap_req_response = ":server.capabilities CAP * ACK :" + client_message.substr(8) + "\r\n";
-						send(client_fd, cap_req_response.c_str(), cap_req_response.length(), 0);
-				} else if (client_message.find("PASS") != std::string::npos) { //NO FUNCIONA
-						// Handle password command
-						if (client_message.substr(5) == password) {
-								std::string pass_response = ":server 001 :Password accepted\r\n";
-								send(client_fd, pass_response.c_str(), pass_response.length(), 0);
-						} else {
-								std::string pass_fail_response = ":server 464 :Password incorrect\r\n";
-								send(client_fd, pass_fail_response.c_str(), pass_fail_response.length(), 0);
-								close(client_fd);
-								break;
-						}
-				} else {
-					std::cout << client_message << std::endl;
-						std::string msg_response = ":server 001 : Unknown holas\r\n";
-						send(client_fd, msg_response.c_str(), msg_response.length(), 0);
-				}
-		}
-}
-
-int main(int argc, char *argv[]) {
-		if (argc != 3) {
-				std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
-				return 1;
-		}
-
-		int port;
-		std::stringstream(argv[1]) >> port;
-		std::string password = argv[2];
-
-		int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-		if (server_fd < 0) {
-				std::cerr << "Failed to create socket" << std::endl;
-				return 1;
-		}
-
-		sockaddr_in server_addr;
-		memset(&server_addr, 0, sizeof(server_addr));
-		server_addr.sin_family = AF_INET;
-		server_addr.sin_addr.s_addr = INADDR_ANY;
-		server_addr.sin_port = htons(port);
-
-		if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-				std::cerr << "Bind failed" << std::endl;
-				close(server_fd);
-				return 1;
-		}
-
-		if (listen(server_fd, 3) < 0) {
-				std::cerr << "Listen failed" << std::endl;
-				close(server_fd);
-				return 1;
-		}
-
-		std::cout << "Server listening on port " << port << std::endl;
-
-		while (true) {
-				sockaddr_in client_addr;
-				socklen_t client_addr_len = sizeof(client_addr);
-				int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
-				if (client_fd < 0) {
-						std::cerr << "Accept failed" << std::endl;
-						continue;
-				}
-
-				std::cout << "Client connected" << std::endl;
-
-				handle_client(client_fd, password);
-		}
-
-		close(server_fd);
-		return 0;
+    return 0;
 }
